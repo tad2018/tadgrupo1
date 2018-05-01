40d8c09401ddac1eca3d8c1425bf272f6388045c
dexpjar <daniel_exja@hotmail.com>
2018-05-01 18:58:06 +0200

    Estilo Index

--- leaguetad/src/main/java/com/mycompany/leaguetad/Index.java
+++ leaguetad/src/main/java/com/mycompany/leaguetad/Index.java
@@ -8,8 +8,10 @@ import com.vaadin.server.Sizeable;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
@@ -22,17 +24,30 @@ import com.vaadin.ui.VerticalLayout;
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
  * overridden to add component to the user interface and initialize non-component functionality.
  */
-@Theme("mytheme")
+@Theme("tests-valo-dark")
 public class Index extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final HorizontalSplitPanel layout = new HorizontalSplitPanel();
+        layout.setStyleName("fondo");
         
         final VerticalLayout clasificacionesLayout = new VerticalLayout();
         final VerticalLayout loginLayout = new VerticalLayout();
         
-        final Label iniciarSesionLabel = new Label("Iniciar Sesión en League TAD");
+        Panel loginPanel = new Panel("Login");
+        loginPanel.setStyleName("panelLogin");
+        loginPanel.setWidth(150, Unit.PIXELS);
+        loginPanel.setHeight(150, Unit.PIXELS);
+        VerticalLayout content = new VerticalLayout();
+        loginPanel.setContent(content);
+        loginPanel.setSizeUndefined();
+
+        // No captions for fields is they are provided in the template
+//        content.addComponent(new TextField(), "username");
+//        content.addComponent(new TextField(), "password");
+//        content.addComponent(new Button("Login"), "okbutton");
+        
         final Label nombreUsuarioLabel = new Label("Nombre Usuario");
         final Label passwordLabel = new Label("Contraseña");
         
@@ -43,8 +58,12 @@ public class Index extends UI {
         buttonIniciarSesion.addClickListener( e -> {
             layout.addComponent(new Label("Hey"));
         });
+        
+        buttonIniciarSesion.setStyleName("buttonSuccess");
 
-        loginLayout.addComponents(iniciarSesionLabel, nombreUsuarioLabel, nombreUsuarioText, passwordLabel, passwordText, buttonIniciarSesion);
+        content.addComponents(nombreUsuarioLabel, nombreUsuarioText, passwordLabel, passwordText, buttonIniciarSesion);
+        content.setSpacing(true);
+        loginLayout.addComponents(loginPanel);
         
 
         final Label liga = new Label("Ligas");
